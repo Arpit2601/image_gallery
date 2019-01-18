b717265a83f06db759005eb33bbfa01ad25cc534
arpit-iitg <arpit170101012@iitg.ac.in>
2019-01-18 16:12:41 +0530

    first commit

--- hello_world/hello_world/Form1.Designer.vb
+++ hello_world/hello_world/Form1.Designer.vb
@@ -22,50 +22,98 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.imageFolderBrowserDlg = New System.Windows.Forms.FolderBrowserDialog()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(80, 163)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(191, 61)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Load Image"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'OpenFileDialog1
         '
         Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
-        'RichTextBox1
+        'TextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(19, 19)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(301, 82)
-        Me.RichTextBox1.TabIndex = 4
-        Me.RichTextBox1.Text = ""
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Monotype Corsiva", 18.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(404, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(474, 35)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "Gallery"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(169, 28)
+        Me.Button2.MaximumSize = New System.Drawing.Size(233, 75)
+        Me.Button2.MinimumSize = New System.Drawing.Size(30, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(229, 75)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "List View"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(885, 28)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.MaximumSize = New System.Drawing.Size(255, 75)
+        Me.Button1.MinimumSize = New System.Drawing.Size(30, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(251, 75)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Grid View"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.AutoSize = True
+        Me.Panel1.Location = New System.Drawing.Point(-4, 109)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1391, 573)
+        Me.Panel1.TabIndex = 7
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = True
-        Me.ClientSize = New System.Drawing.Size(1052, 532)
-        Me.Controls.Add(Me.RichTextBox1)
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(1399, 735)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MinimumSize = New System.Drawing.Size(1400, 770)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-    End Sub
-    Friend WithEvents Button1 As Button
+End Sub
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
     Friend WithEvents imageFolderBrowserDlg As FolderBrowserDialog
-    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
 End Class
--- hello_world/hello_world/Form1.vb
+++ hello_world/hello_world/Form1.vb
@@ -3,40 +3,16 @@
 Public Class Form1
     Private imageFiles() As String = Nothing
     Dim bmp01, bmp02, bmpTemp As Bitmap
-    Dim clr, clrTemp As Color
-    Dim intR, intG, intB, intTemp As Integer
-    Dim intResponse As Integer
-
-
-
-    Dim fraction As Single
-    ' Sub is subroutine and doen not return any value
-
+    Dim picBox As New PictureBox
+    Dim textBox As New TextBox
 
     Private Sub resize_image(ByRef bmp01 As Bitmap)
-        'OpenFileDialog1.Filter = "jpeg files|*.jpeg|bitmap files|*.bmp|jpg files|*.jpg|gif files|*.gif"
 
-        'If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
-        'bmp01 = New Bitmap(Image.FromFile(string_name))
 
-        If bmp01.Width > 100 Or bmp01.Height > 100 Then
-            'intResponse = MsgBox("image is larger than display area. do you want to resize image to fit?", vbYesNo + vbQuestion)
-            'End If
-
-            'If intResponse = vbNo Then
-            'Exit Sub
-            'End If
-
-            'If intResponse = vbYes Then
-            'If (bmp01.Width) / 4 >= (bmp01.Height) / 3 Then
-            'fraction = 1 / ((bmp01.Width) / 640)
-            'Else
-            'fraction = 1 / ((bmp01.Height) / 480)
-            'End If
-            bmpTemp = New Bitmap(bmp01, 100, 100)
+        If bmp01.Width > 150 Or bmp01.Height > 150 Then
+            bmpTemp = New Bitmap(bmp01, 150, 150)
             bmpTemp.SetResolution(bmp01.HorizontalResolution, bmp01.VerticalResolution)
             bmp01 = bmpTemp
-            'PictureBox1.Image = bmp01
         End If
 
 
@@ -44,36 +20,29 @@ Public Class Form1
     End Sub
     Private Sub button1_click(sender As Object, e As EventArgs) Handles Button1.Click
         If imageFolderBrowserDlg.ShowDialog() = DialogResult.OK Then
-            Me.imageFiles = GetFiles(Me.imageFolderBrowserDlg.SelectedPath, "*.jpg;*.jpeg;*.png;*.bmp;*.tif;*.tiff;*.gif")
+            Me.imageFiles = GetFiles(Me.imageFolderBrowserDlg.SelectedPath, "*.jpg;*.jpeg;*.png;*.bmp;*.tif;*.tiff;*.gif;*.JPG")
             If Me.imageFiles.Length = 0 Then
                 MessageBox.Show("No image can be found")
             Else
-                ' show images using graphics and bitmap
-                Dim count As Integer
 
-                Dim bm2 = New Bitmap(10, 10)
+
+
+                ' Dim bm2 = New Bitmap(10, 10)
                 Dim image_string1 As String
 
                 Dim XLocation As Integer
                 Dim i As Integer
 
                 Dim YLocation As Integer
-                XLocation = 365
-                YLocation = 20
+                XLocation = 20
+                YLocation = 185
 
                 For Each image_string As String In imageFiles
-                    ' TextBox1.Text &= image_string & Environment.NewLine
-                    count += 1
+
+
                     image_string1 = image_string
                     DrawPictureBox(image_string, XLocation, YLocation, i)
-
-
-
                 Next
-
-
-
-
             End If
         End If
 
@@ -82,33 +51,52 @@ Public Class Form1
 
     Public Sub DrawPictureBox(ByVal string_name As String, ByRef x As Integer, ByRef y As Integer, ByRef i As Integer)
         Dim picBox As New PictureBox
+        Dim bmp01 As New Bitmap(150, 150)
         picBox.Location = New Point(x, y)
-        bmp01 = New Bitmap(Image.FromFile(string_name))
+        Try
 
-        x += 120
+            bmp01 = Image.FromFile(string_name) ' add system.memorylimitexceeded exception
+        Catch e As System.OutOfMemoryException
+            Console.WriteLine("system out of memory")
+            'Throw New System.OutOfMemoryException()
+            'MessageBox.Show("you have selected too many images")
+            'Me.Close()
+            'Me.Show()
+        End Try
+        Dim textBox As New TextBox
+        textBox.Location = New Point(x, y + 155)
+        x += 170
         resize_image(bmp01)
-        If x + 100 >= 1050 Then
-            x = 365
-            y = y + 120
+        If x + 150 >= Me.Width Then
+            x = 30
+            y = y + 195
         End If
 
+        Dim picture_name As String = System.IO.Path.GetFileName(string_name)
+        textBox.Name = "TextBox" & i
+        textBox.Size = New Size(150, 20)
+        textBox.Text = picture_name
+        textBox.BackColor = Me.BackColor
+        textBox.BorderStyle = BorderStyle.None
+        textBox.ReadOnly = True
+        textBox.Anchor = AnchorStyles.Left And AnchorStyles.Right
+        Me.Controls.Add(textBox)
 
-        Dim a As Integer
-        Dim b As Integer
-        '(a,b) = bmp01.Size()
-        a = bmp01.Width
-        b = bmp01.Height
 
-        ' Debug.Print(x, y)
         picBox.Name = "PictureBox" & i
         i += 1
-        picBox.Size = New Size(100, 100)
+        picBox.Size = New Size(150, 150)
         picBox.TabIndex = 0
         picBox.TabStop = False
         picBox.BorderStyle = BorderStyle.Fixed3D
+        picBox.Anchor = AnchorStyles.Left And AnchorStyles.Right
+        picBox.Tag = string_name
         picBox.ImageLocation = string_name
-        Me.Controls.Add(picBox)
+        Me.Panel1.Controls.Add(picBox)
+        ' Me.Controls.Add(picBox)
         picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        AddHandler picBox.MouseDoubleClick, AddressOf picBox_DoubleClick
+        bmp01.Dispose()
 
     End Sub
 
@@ -117,70 +105,41 @@ Public Class Form1
 
         Dim files As New List(Of String)()
         For Each filter As String In patterns
-            ' Iterate through the directory tree and ignore the  
-            ' DirectoryNotFoundException or UnauthorizedAccessException  
-            ' exceptions.  
-            ' http://msdn.microsoft.com/en-us/library/bb513869.aspx 
+            
 
-            ' Data structure to hold names of subfolders to be 
-            ' examined for files. 
-            Dim dirs As New Stack(Of String)(20)
 
             If Not IO.Directory.Exists(path) Then
                 Throw New ArgumentException()
             End If
-            dirs.Push(path)
+            
+            Try
+                files.AddRange(IO.Directory.GetFiles(path, filter))
+            Catch e3 As UnauthorizedAccessException
+                Throw New UnauthorizedAccessException
 
-            Do While dirs.Count > 0
-                Dim currentDir As String = dirs.Pop()
-                Dim subDirs() As String
-                Try
-                    subDirs = IO.Directory.GetDirectories(currentDir)
-                    ' An UnauthorizedAccessException exception will be thrown  
-                    ' if we do not have discovery permission on a folder or  
-                    ' file. It may or may not be acceptable to ignore the  
-                    ' exception and continue enumerating the remaining files  
-                    ' and folders. It is also possible (but unlikely) that a  
-                    ' DirectoryNotFound exception will be raised. This will  
-                    ' happen if currentDir has been deleted by another  
-                    ' application or thread after our call to Directory.Exists.  
-                    ' The choice of which exceptions to catch depends entirely  
-                    ' on the specific task you are intending to perform and  
-                    ' also on how much you know with certainty about the  
-                    ' systems on which this code will run. 
-                Catch e1 As UnauthorizedAccessException
-                    Continue Do
-                Catch e2 As IO.DirectoryNotFoundException
-                    Continue Do
-                End Try
+            Catch e4 As IO.DirectoryNotFoundException
+                Throw New ArgumentException()
+            End Try
 
-                Try
-                    files.AddRange(IO.Directory.GetFiles(currentDir, filter))
-                Catch e3 As UnauthorizedAccessException
-                    Continue Do
-                Catch e4 As IO.DirectoryNotFoundException
-                    Continue Do
-                End Try
-
-                ' Push the subdirectories onto the stack for traversal. 
-                ' This could also be done before handing the files. 
-                For Each str As String In subDirs
-                    dirs.Push(str)
-                Next str
-            Loop
         Next filter
 
         Return files.ToArray()
     End Function
 
+    Private Sub picBox_DoubleClick(ByVal sender As Object, ByVal e As EventArgs)
+        Dim Form2Caller As New FullScreen
+        Form2Caller.SetPictureBoxImage(sender.Tag)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+
+    Private Sub VScrollBar1_Scroll(sender As Object, e As ScrollEventArgs)
+        AutoScroll = True
+    End Sub
 End Class
 
 
-' Using g As Graphics = Graphics.FromImage(bm2)
-'g.DrawImage(bm2, 0, 0, 10, 10)
-'End Using
-' PictureBox1.Image = bm
-
-'Using g As Graphics = Graphics.FromImage(PictureBox1)
-
-'End Using
+'
