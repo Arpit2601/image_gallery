a33a70885d250ce29d6da0d2e044fde8ad57af0e
karlitowalker <ajaiswal9043@gmail.com>
2019-01-21 18:11:58 +0530

    added background

--- hello_world/hello_world/My Project/Resources.Designer.vb
+++ hello_world/hello_world/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property background_blur_clean_531880_1574x573() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("background-blur-clean-531880_1574x573", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property icons8_flip_horizontal_32() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("icons8-flip-horizontal-32", resourceCulture)
